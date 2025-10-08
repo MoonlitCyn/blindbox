6a8757805e51de58c55189fc32ad9eed9d0a94d1
Cynthia <cynthia.chen@student.hu.nl>
2025-10-08 11:21:10 +0200

    update

--- js/utils.js
+++
@@ -1,7 +0,0 @@
-// 小工具：格式化时间
-export function nowStr() {
-  const d = new Date();
-  const pad = n => String(n).padStart(2, "0");
-  return `${d.getFullYear()}-${pad(d.getMonth()+1)}-${pad(d.getDate())} `
-       + `${pad(d.getHours())}:${pad(d.getMinutes())}:${pad(d.getSeconds())}`;
-}
